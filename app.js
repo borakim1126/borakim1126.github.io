a922a6272ac6a3533d228038a4458371ecbd885e
Bora.Kim <borakim@Boras-MacBook-Pro.local>
2016-03-31 19:43:35 -0400

    hi

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ $(function(){
     }
   };
 
-  if(!PDK.getSession().accessToken){
-     $('.image-results-view').hide();
-  } else {
-     //do your thing
+  if(PDK.getSession().accessToken){
+    //do your thing
     $('.sign-in-view').hide();
     var token = PDK.getSession();
     var url = "https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=4826032574203707802&client_secret=956a6143b1e798674eecdb4e5e9c1b58f9cadfcc6f2fd02094116ab8a0b2630d&code="+token.accessToken;
 
+  } else {
+         $('.image-results-view').hide();
   }
 //  ○ Search Pinterest posts by hastag / poster / board /color
 // ○ Shoppable pins
