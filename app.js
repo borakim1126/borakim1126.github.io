6d72996c80de9ebef565586d57fdf40875221362
Bora.Kim <borakim@Boras-MacBook-Pro.local>
2016-03-30 20:29:36 -0400

    setting access token and url

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ function isHashPresent() {
 if(isHashPresent()) {
    $('.sign-in-view').hide();
   //navigator.geolocation.getCurrentPosition(function(pos){
-    var lat = pos.coords.latitude;
-    var long = pos.coords.longitude;
-    var accessToken = window.location.hash.replace('#access_token=', '')
+    //var lat = pos.coords.latitude;
+    //var long = pos.coords.longitude;
+    var accessToken = window.location.search.replace('?state=768uyFys&code=', '')
     var url = "https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=4826032574203707802&client_secret=956a6143b1e798674eecdb4e5e9c1b58f9cadfcc6f2fd02094116ab8a0b2630d&code="+accessToken;
 
     // url = url + "&lat=" + lat;
@@ -33,3 +33,8 @@ if(isHashPresent()) {
   $('.sign-in-view').show();
   $('.image-results-view').hide();
 }
+
+
+//  ○ Search Pinterest posts by hastag / poster / board /color
+// ○ Shoppable pins
+//  ○ Order by number of pins / likes
