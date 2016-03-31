3d8429241f7954befa08358d445fda26a3af7bb3
Bora.Kim <borakim@Boras-MacBook-Pro.local>
2016-03-31 19:21:10 -0400

    asdfa

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 $(function(){
-  
-  var token = PDK.getSession();
-  if(token.accessToken){
-     $('.sign-in-view').hide();
-  } else {
-    //do your thing
-    var url = "https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=4826032574203707802&client_secret=956a6143b1e798674eecdb4e5e9c1b58f9cadfcc6f2fd02094116ab8a0b2630d&code="+token;
-    
-  }
 
   var clickMe = $(".btn-default")
 
@@ -23,6 +14,15 @@ $(function(){
       console.error(resp.error);
     } else if(resp.session) {
       console.info(resp.session);
+      var token = PDK.getSession();
+      if(token.accessToken){
+        $('.sign-in-view').hide();
+        var url = "https://api.pinterest.com/v1/oauth/token?grant_type=authorization_code&client_id=4826032574203707802&client_secret=956a6143b1e798674eecdb4e5e9c1b58f9cadfcc6f2fd02094116ab8a0b2630d&code="+token;
+ 
+      } else {
+        //do your thing
+         }
+
     }
   };
 
